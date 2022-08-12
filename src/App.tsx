2d30140fa696a897ab54dc9304b83d333a935eb3
Andrew Palmer <andrew.palmer.10@gmail.com>
2022-08-12 12:02:04 -0400

    Working solution finder

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,35 @@ const MAZE: number[][] = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 const DEFAULT_LOCATION: [number, number] = [-1, -1];
+
 function App() {
+  const [maze, setMaze] = useState(MAZE);
   const [start, setStart] = useState<[number, number]>(DEFAULT_LOCATION);
   const [end, setEnd] = useState<[number, number]>(DEFAULT_LOCATION);
 
   const clearState = () => {
+    setMaze([...MAZE]);
     setStart(DEFAULT_LOCATION);
     setEnd(DEFAULT_LOCATION);
+  };
+
+  const fakeSet: any = {};
+
+  const seen = (row: number, col: number) => {
+    if (row in fakeSet && fakeSet[row].has(col)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const add = (row: number, col: number) => {
+    if (row in fakeSet) {
+      fakeSet[row].add(col);
+    } else {
+      fakeSet[row] = new Set();
+      fakeSet[row].add(col);
+    }
   };
 
   const handleClick = (row: number, col: number) => {
@@ -37,10 +59,54 @@ function App() {
     }
   };
 
+  const solveMaze = async () => {
+    if (start === DEFAULT_LOCATION || end === DEFAULT_LOCATION) {
+      return;
+    }
+    const q: [number, number, [number, number][]][] = [[...start, []]];
+
+    while (q.length > 0) {
+      const [rowIndex, colIndex, path] = q[0];
+      q.shift();
+
+      if (seen(rowIndex, colIndex)) continue;
+      add(rowIndex, colIndex);
+
+      if (rowIndex < 0 || rowIndex === MAZE.length) continue;
+      if (colIndex < 0 || colIndex === MAZE[0].length) continue;
+
+      maze[rowIndex][colIndex] = 1;
+      setMaze([...maze]);
+      await new Promise((r) => setTimeout(r, 25));
+
+      const updatedPath: [number, number][] = [...path, [rowIndex, colIndex]];
+
+      if (rowIndex === end[0] && colIndex === end[1]) {
+        const newMaze = [...maze];
+        path.forEach((cell) => {
+          newMaze[cell[0]][cell[1]] = 2;
+        });
+        setMaze(newMaze);
+        break;
+      }
+
+      if (!seen(rowIndex + 1, colIndex))
+        q.push([rowIndex + 1, colIndex, updatedPath]);
+      if (!seen(rowIndex - 1, colIndex))
+        q.push([rowIndex - 1, colIndex, updatedPath]);
+      if (!seen(rowIndex, colIndex + 1))
+        q.push([rowIndex, colIndex + 1, updatedPath]);
+      if (!seen(rowIndex, colIndex - 1))
+        q.push([rowIndex, colIndex - 1, updatedPath]);
+    }
+
+    setMaze(maze);
+  };
+
   return (
     <div className="App">
       <div className="maze-container">
-        {MAZE.map((row, rowIndex) => (
+        {maze.map((row, rowIndex) => (
           <div className="row" key={rowIndex}>
             {row.map((_, colIndex) => (
               <Cell
@@ -48,13 +114,15 @@ function App() {
                   start ? rowIndex === start[0] && colIndex === start[1] : false
                 }
                 isEnd={end ? rowIndex === end[0] && colIndex === end[1] : false}
+                visited={maze[rowIndex][colIndex] === 1}
+                partOfSolution={maze[rowIndex][colIndex] === 2}
                 key={colIndex}
                 handleClick={() => handleClick(rowIndex, colIndex)}
               />
             ))}
           </div>
         ))}
-        <button>solve</button>
+        <button onClick={solveMaze}>solve</button>
         <button onClick={clearState}>clear</button>
       </div>
     </div>
--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -6,10 +6,20 @@ interface Props {
   handleClick: () => void;
   isEnd: boolean;
   isStart: boolean;
+  visited: boolean;
+  partOfSolution: boolean;
 }
 
-const Cell: React.FC<Props> = ({ handleClick, isEnd, isStart }) => {
-  const className = `cell ${isStart ? "start" : ""} ${isEnd ? "end" : ""}`;
+const Cell: React.FC<Props> = ({
+  handleClick,
+  isEnd,
+  isStart,
+  partOfSolution,
+  visited,
+}) => {
+  const className = `cell ${visited ? "visited" : ""} ${
+    isStart ? "start" : ""
+  } ${isEnd ? "end" : ""} ${partOfSolution ? "solution" : ""}`;
   return <div className={className} onClick={handleClick} />;
 };
 
